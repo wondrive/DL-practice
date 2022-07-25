c8ce9a3c59db58944fddc622e2fba38b970a89d6
Won Jeong <jswfs1284@gmail.com>
2022-07-25 23:21:55 +0900

    Add files via upload

---
+++ plot_roc.py
@@ -0,0 +1,94 @@
+import torch
+import matplotlib.pyplot as plt
+from collections import Counter
+
+
+def plot_roc(pred_boxes, true_boxes, num_classes, iou_threshold=0.5, box_format="corners"):
+     classes = {0:'사과', 1:'포도', 2:'배'}
+     
+     AUCs = []
+     TPRs = []
+     FPRs = []
+
+     epsilon = 1e-6
+
+     for c in range(num_classes):
+          detections = []
+          ground_truths = []
+          
+          for detection in pred_boxes:
+               if detection[1] == c:       # class == c
+                    detections.append(detection)
+
+          for true_box in true_boxes:
+               if true_box[1] == c:
+                    ground_truths.append(true_box)
+                    
+          amount_bboxes = Counter([gt[0] for gt in ground_truths])
+
+          for key, val in amount_bboxes.items():
+               amount_bboxes[key] = torch.zeros(val)
+
+          detections.sort(key=lambda x: x[2], reverse=True)
+          TP = torch.zeros((len(detections)))
+          FP = torch.zeros((len(detections)))
+          total_true_bboxes = len(ground_truths)
+          
+          if total_true_bboxes == 0:
+               continue
+          
+          for detection_idx, detection in enumerate(detections):
+               ground_truth_img = [
+                    bbox for bbox in ground_truths if bbox[0] == detection[0]
+               ]
+
+               num_gts = len(ground_truth_img)
+               best_iou = 0
+
+               for idx, gt in enumerate(ground_truth_img):
+                    '''
+                    intersection_over_union() 함수는 다음 github 오픈소스 참고
+                    github @aladdinpersson : https://github.com/aladdinpersson/Machine-Learning-Collection/blob/master/ML/Pytorch/object_detection/metrics/iou.py
+                    '''
+                    iou = intersection_over_union(
+                         torch.tensor(detection[3:]),
+                         torch.tensor(gt[3:]),
+                         box_format=box_format,
+                    )
+                    
+                    if iou > best_iou:
+                         best_iou = iou
+                         best_gt_idx = idx
+
+               if best_iou > iou_threshold:
+                    if amount_bboxes[detection[0]][best_gt_idx] == 0:
+                         TP[detection_idx] = 1
+                         amount_bboxes[detection[0]][best_gt_idx] = 1
+                    else:
+                         FP[detection_idx] = 1
+               else:
+                    FP[detection_idx] = 1
+
+          TP_cumsum = torch.cumsum(TP, dim=0)     # 누적합
+          FP_cumsum = torch.cumsum(FP, dim=0)
+          TN_cumsum = FP_cumsum[-1] - FP_cumsum
+          
+          TPR = TP_cumsum / (total_true_bboxes + epsilon) # sensitivity
+          FPR = torch.div(FP_cumsum, (TN_cumsum + FP_cumsum + epsilon)) # 1 - specificity
+          TPRs.append(TPR)
+          FPRs.append(FPR)
+          AUCs.append(torch.trapz(TPR, FPR))
+          
+     # plot ROC curves
+     plt.figure(figsize=(15, 5))
+     for i in range(num_classes):
+          plt.subplot(131+i)
+          plt.plot(FPRs[i], TPRs[i], color='red', label=classes[i])
+          plt.plot([0, 1], [0, 1], color='black', linestyle='--')
+          plt.xlim([0.0, 1.0])
+          plt.ylim([0.0, 1.05])
+          plt.xlabel('FPR')
+          plt.ylabel('TPR')
+          plt.title(f'ROC curve ({classes[i]})\nAUC: {AUCs[i]:.2f}')
+          plt.legend(loc='lower right')
+     plt.show()
